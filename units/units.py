b5bf17641f733a2177fe387e37659fa749415b05
Inti Pelupessy <ipelupessy@users.noreply.github.com>
2015-06-02 13:58:06 +0200

    generator for roberts displaced pole grid

--- units/units.py
+++ units/units.py
@@ -1,7 +1,10 @@
 from amuse.units.units import *
 from amuse.units.core import named_unit
+from amuse.units.quantities import zero
 
 Sv=named_unit("Sverdrup","Sv",1.e6*m**3/s)
 dyn=named_unit("Dyne","dyn", 1.e-5*N)
+bar=named_unit("Bar", "bar", 1.e5*Pa)
+
 
 psu=named("psu","psu", none)
