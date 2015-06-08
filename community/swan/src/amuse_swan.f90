a14a38daf33c4601eb203d64ab20e96494c396a5
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2015-06-08 11:37:14 +0200

    initial swan

---
+++ community/swan/src/amuse_swan.f90
@@ -0,0 +1,20 @@
+module amuse_swan
+  USE TIMECOMM                                                        
+  USE OCPCOMM1                                                        
+  USE OCPCOMM2                                                        
+  USE OCPCOMM3                                                        
+  USE OCPCOMM4                                                        
+  USE SWCOMM1                                                         
+  USE SWCOMM2                                                         
+  USE SWCOMM3                                                         
+  USE SWCOMM4                                                         
+  USE OUTP_DATA                                                       
+  USE M_SNL4                                                          
+  USE M_GENARR                                                        
+  USE M_OBSTA                                                         
+  USE M_PARALL                                                        
+  USE SwanGriddata                                                    
+
+contains
+
+end module
