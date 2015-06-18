80cbb462452dea9ba6de39710de289cc15215d20
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2015-06-18 15:18:02 +0200

    dbar unit

--- units/units.py
+++ units/units.py
@@ -5,6 +5,6 @@ from amuse.units.quantities import zero
 Sv=named_unit("Sverdrup","Sv",1.e6*m**3/s)
 dyn=named_unit("Dyne","dyn", 1.e-5*N)
 bar=named_unit("Bar", "bar", 1.e5*Pa)
-
+dbar=deci(bar)
 
 psu=named("psu","psu", none)
