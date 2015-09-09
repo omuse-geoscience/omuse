19649f286e2bfda8ecbd0272bc10608ea5af79a1
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2015-09-09 10:46:25 +0200

    continuing the implementation of the remapper on top of CDO

--- community/cdo/interface.cc
+++ community/cdo/interface.cc
@@ -88,6 +88,92 @@ int get_dst_grid_size(int * size) {
   return 0;
 }
 
+int get_src_grid_dims(int *x, int *y) {
+  *x = src_grid.dims[0];
+  *y = src_grid.dims[1];
+  return 0;
+}
+int get_dst_grid_dims(int *x, int *y) {
+  *x = dst_grid.dims[0];
+  *y = dst_grid.dims[1];
+  return 0;
+}
+
+
+
+/*
+ * It would appear that CDO leaves the grid masks empty in the weights
+ * file, even if they are supplied when the weights were generated.
+ */
+int get_src_grid_mask(int *index_i, int *index_j, int *imask, int n) {
+  int i;
+  int nx = src_grid.dims[0];
+  int ny = src_grid.dims[1];
+
+  printf("get_src_grid_mask() called\n");
+  printf("nx=%d, ny=%d, n=%d",nx, ny, n);
+
+  for (i=0; i<n; i++) {
+      imask[index_i[i]*nx+index_j[i]] = src_grid.vgpm[index_i[i]*nx+index_j[i]];
+  }
+
+  return 0;
+}
+
+
+
+
+
+//gradients used for second order and bicubic remappings
+//to be computed when the weights are read in or computed
+double *src_grad1 = NULL;
+double *src_grad2 = NULL;
+double *src_grad3 = NULL;
+
+
+int perform_remap(int *index_i, double *dst, double *src, int n) {
+
+  /*
+    This function calls the remapping function in CDO, it also performs some preprocessing and checks that
+    would otherwise be performed by the Remap() function that implements the operator
+
+    Arguments: 
+        index_i is a list of indexes in the destination grid for which a remapped value from the source grid is requested
+        dst is the array that contains the result of this remapping
+        src is the array that contains the source grid to be remapped
+        n is the number of grid points to be remapped, which should be equal to the number of grid points in the destination grid
+
+    For now partial remappings are not supported n must equal dst grid size.
+
+
+    void remap(double *restrict dst_array, double missval, long dst_size, long num_links, double *restrict map_wts,
+       long num_wts, const int *restrict dst_add, const int *restrict src_add, const double *restrict src_array,
+       const double *restrict src_grad1, const double *restrict src_grad2, const double *restrict src_grad3,
+       remaplink_t links)
+    Input:
+    int *dst_add         ! destination address for each link
+    int *src_add         ! source      address for each link
+    int num_wts          ! num of weights used in remapping
+    double *map_wts      ! remapping weights for each link
+    double *src_array    ! array with source field to be remapped
+    optional inputs:
+    double *src_grad1    ! gradient arrays on source grid necessary for
+    double *src_grad2    ! higher-order remappings
+    double *src_grad3
+    output variables:
+    Output:
+    double *dst_array    ! array for remapped field on destination grid
+  */
+
+  double missval = 0.0; //have to find out what this is exactly
+
+  remap(dst, missval, dst_grid.size, rv.num_links, rv.wts,
+       rv.num_wts, rv.tgt_cell_add, rv.src_cell_add, src,
+       src_grad1, src_grad2, src_grad3,
+       rv.links);
+
+  return 0;
+}
 
 
 
@@ -167,9 +253,13 @@ void print_remap_info(remapvars_t *rv) {
   printf("resize_increment %d\n", rv->resize_increment); // default amount to increase array size    
 
 //  int*     src_cell_add;     // source grid address for each link        
+  printf("rv->src_cell_add %p\n", rv->src_cell_add);
+
 //  int*     tgt_cell_add;     // target grid address for each link        
+  printf("rv->tgt_cell_add %p\n", rv->tgt_cell_add);
 
 //  double*  wts;              // map weights for each link [max_links*num_wts] 
+  printf("rv->wts %p\n", rv->wts);
 
   //remaplinks_t links
   printf("links.option %d\n",      rv->links.option);
@@ -181,8 +271,22 @@ void print_remap_info(remapvars_t *rv) {
 
 
 //  int**    rv->links.src_add;
+  printf("rv->links.src_add %p\n", rv->links.src_add);
+  if (rv->links.src_add) {
+    printf("rv->links.src_add %p\n", *(rv->links.src_add));
+  }
+
 //  int**    rv->links.dst_add;
+  printf("rv->links.dst_add %p\n", rv->links.dst_add);
+  if (rv->links.dst_add) {
+    printf("rv->links.dst_add %p\n", *(rv->links.dst_add));
+  }
+
 //  int**    rv->links.w_index;
+  printf("rv->links.w_index %p\n", rv->links.w_index);
+  if (rv->links.w_index) {
+    printf("rv->links.w_index %p\n", *(rv->links.w_index));
+  }
 
 
   printf("\n");
