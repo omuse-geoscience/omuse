3290e7b7efe29391d7f885822bd226de58154d4e
Ben van Werkhoven <2337546+benvanwerkhoven@users.noreply.github.com>
2015-10-06 11:43:44 +0200

    added the adcirc grid to the repository and further updates of the CDO interface

---
+++ community/cdo/spherical_geometry.py
@@ -0,0 +1,41 @@
+import numpy
+from math import *
+
+#radius of Earth in meters
+R = 6371000
+
+#returns the spherical distance between two coordinates in given in lat,lon
+#result should be multiplied by R for the actual distance in meters
+def distance(lat1, lon1, lat2, lon2):
+    dlat = lat2-lat1
+    dlon = lon2-lon1
+
+    #using haversine formula
+    a = sin(dlat/2.0) * sin(dlat/2.0) + \
+        cos(lat1) * cos(lat2) * \
+        sin(dlon/2.0) * sin(dlon/2)
+
+    d = 2 * atan2(sqrt(a), sqrt(1.0-a))
+
+    return d
+
+#returns the area of the triangle enclosed by three lines of distances a, b, and c
+#result should be multiplied with R**2 for the actual surface area in meters
+def triangle_area(a, b, c):
+    #compute the semiperimeter
+    s = (a + b + c)/2
+
+    #compute spherical excess using L'Huilier's Theorem
+    tans = tan(s/2)
+    tana = tan((s-a)/2)
+    tanb = tan((s-b)/2)
+    tanc = tan((s-c)/2)
+
+    tanE4 = sqrt(tans * tana * tanb * tanc)
+
+    E = 4 * atan(tanE4)
+
+    return E
+
+
+
--- community/cdo/test/test_CDOInterface.py
+++ community/cdo/test/test_CDOInterface.py
@@ -51,24 +51,3 @@ class CDOInterfaceTests(TestWithMPI):
         instance.stop()
 
 
-    @nottest
-    def test4(self):
-        cdo = CDOInterface(**default_options)
-        cdo.set_remap_file("weights/src_dst_con.nc")
-
-        src_dims = cdo.get_src_grid_dims()['dims']
-        dst_dims = cdo.get_dst_grid_dims()['dims']
-        
-        src = [[0 for x in range(src_dims['x'])] for y in range(src_dims['y'])]  # 180x90
-        dst = [[0 for x in range(dst_dims['x'])] for y in range(dst_dims['y'])]  # 192x94
-
-        for i in range (src_dims['x']):
-            for j in range (src_dims['y']):
-                src[i][j] = (i/10+j/10) % 2
-
-        #still writing this test
-
-        instance.stop()
-
-
-
