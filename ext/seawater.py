5ef3f0f572191d96e85057f8dce16079025b1bb3
Inti Pelupessy <ipelupessy@users.noreply.github.com>
2015-10-24 22:08:23 +0200

    correct unit celsius

--- ext/seawater.py
+++ ext/seawater.py
@@ -5,14 +5,14 @@ import _seawater
 def dens(S,T,P=0. | units.dbar):
     """  Density of sea water """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.dens(_S, _T, _P) | units.kg/units.m**3
 
 def svan(S,T,P=0. | units.dbar):
     """ Specific volume anomaly """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.svan(_S,_T,_P) | units.m**3/units.kg
 delta=svan
@@ -20,49 +20,49 @@ delta=svan
 def sigma(S,T,P=0. | units.dbar):
     """ Density anomaly """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.sigma(_S,_T,_P) | units.kg/units.m**3
 
 def drhodt(S,T,P=0. | units.dbar):
     """Temperature derivative of density"""
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.drhodt(_S,_T,_P) | units.kg/(units.K*units.m**3)
 
 def alpha(S,T,P=0. | units.dbar):
     """ Thermal expansion coefficient """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.alpha(_S,_T,_P) | units.K**-1
     
 def drhods(S,T,P=0. | units.dbar):
     """ Salinity derivative of density """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.drhods(_S,_T,_P) | units.kg/units.m**3
 
 def beta(S,T,P=0. | units.dbar):
     """ Salinity expansion coefficient """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.beta(_S,_T,_P)
 
 def salt(R_cond, T, P=0. | units.dbar):
     """ Salinity """
     _R=R_cond
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.salt( _R,_T,_P)
 
 def R_cond(S,T,P=0. | units.dbar):
     """ Conductivity ratio """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.cond(_S,_T,_P)
 cond=R_cond
@@ -70,42 +70,42 @@ cond=R_cond
 def heatcap(S,T,P=0. | units.dbar):
     """ Heat capacity """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.heatcap( _S,_T,_P) | units.J/(units.kg*units.K)
     
 def adtgrad(S,T,P=0. | units.dbar):
     """ Adiabatic lapse rate """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)   
     return _seawater.adtgrad( _S,_T,_P) | units.K/units.dbar
     
 def temppot(S,T,P=0. | units.dbar ,Pref=0. | units.dbar):
     """ Potential temperature """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)
     _Pref=Pref.value_in(units.dbar)
-    return _seawater.temppot( _S,_T,_P,_Pref) | units.C
+    return _seawater.temppot( _S,_T,_P,_Pref) | units.Celsius
 
 def temppot0(S,T,P=0. | units.dbar):
     """ Potential temperature """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)
-    return _seawater.temppot0( _S,_T,_P) | units.C
+    return _seawater.temppot0( _S,_T,_P) | units.Celsius
 
 def freezept(S,P=0. | units.dbar):
     """ Freezing point """
     _S=S.value_in(units.psu)
     _P=P.value_in(units.dbar)
-    return _seawater.freezept(_S,_P) | units.C
+    return _seawater.freezept(_S,_P) | units.Celsius
 
 def soundvel(S,T,P=0. | units.dbar):
     """ Sound velocity  """
     _S=S.value_in(units.psu)
-    _T=T.value_in(units.C)
+    _T=T.value_in(units.Celsius)
     _P=P.value_in(units.dbar)
     return _seawater.soundvel(_S,_T,_P) | units.m/units.s
     
@@ -115,7 +115,7 @@ def depth(P,lat):
     _lat=to_deg(lat)
     return _seawater.depth(_P,_lat) | units.m
    
-def EOS_linear(S, T, rho0=1028. | units.kg/units.m**3, T0=10. | units.C, S0=35. | units.psu, alpha=0.00017 | units.C**-1, beta=0.00076 | units.psu**-1):
+def EOS_linear(S, T, rho0=1028. | units.kg/units.m**3, T0=10. | units.Celsius, S0=35. | units.psu, alpha=0.00017 | units.Celsius**-1, beta=0.00076 | units.psu**-1):
     """
     the equation of state given in Cushman-Roisin, B., Introduction to Geophysical Fluid Dynamics, Prentice-Hall, 1994, 320 pp. (1994)
     (as reported by ADCIRC)
@@ -130,7 +130,7 @@ def EOS_UNESCO(S,T, P=0 | units.Pa):
 
 if __name__=="__main__":
   S=35. | units.psu
-  T=10 | units.C
+  T=10 | units.Celsius
   P=0. | units.dbar
   print EOS_linear( S,T)
   print EOS_linear.__doc__
