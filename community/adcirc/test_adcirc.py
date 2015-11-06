3ab352a7aeb8e4bd47950ec7483c2540290ef22d
Inti Pelupessy <ipelupessy@users.noreply.github.com>
2015-11-06 13:26:48 +0100

    setters for 2d restart variables

--- community/adcirc/test_adcirc.py
+++ community/adcirc/test_adcirc.py
@@ -17,6 +17,8 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("code").setLevel(logging.DEBUG)
 
+from omuse.community.adcirc.read_grid import adcirc_grid_reader, adcirc_parameter_reader
+from omuse.community.adcirc.write_grid import adcirc_grid_writer, adcirc_parameter_writer
 
 class TestAdcircInterface(TestWithMPI):
 
@@ -203,6 +205,115 @@ class TestAdcirc(TestWithMPI):
 
         self.assertEquals(instance.elements.status, u'wet')
         
-        print instance.nodes.eta,instance.nodes.eta_prev
-
         instance.stop()
+
+    def test11(self):
+        instance = Adcirc(**default_options)
+        instance.set_rootdir("data/test/2d")
+
+        instance.evolve_model( 1 | units.hour)
+        self.assertEqual(instance.model_time, 21*174.656| units.s)
+        self.assertEquals(instance.nodes.status, u'wet')
+        self.assertEquals(instance.elements.status, u'wet')
+        
+        instance.nodes[12].status=u'dry'
+        self.assertEqual(instance.nodes[12].status,u'dry')
+
+        instance.nodes[12].eta_prev=0.1 | units.m
+        self.assertEqual(instance.nodes[12].eta_prev,0.1 | units.m)
+
+        instance.nodes[12].eta=0.21 | units.m
+        self.assertEqual(instance.nodes[12].eta,0.21 | units.m)
+
+        instance.nodes[21].vx=0.21 | units.m/units.s
+        self.assertEqual(instance.nodes[21].vx,0.21 | units.m/units.s)
+        instance.nodes[21].vy=0.21 | units.m/units.s
+        self.assertEqual(instance.nodes[21].vy,0.21 | units.m/units.s)
+
+
+        
+        
+        instance.stop()
+
+
+class TestAdcircLong(TestWithMPI):
+
+
+    def test1(self):
+        tend=5*86400. | units.s
+        AMIG=0.000140525700000 | units.s**-1
+        PER=2*numpy.pi/AMIG
+        EMO=0.3048 | units.m
+        DRAMP=2 | units.day
+
+        def ramp(t):
+            return numpy.tanh(2*t/DRAMP)
+
+        def tidal_force_function(t):
+            ncyc=numpy.floor(t/PER)
+            aj=AMIG*(t-ncyc*PER)
+            return ramp(t)*EMO*numpy.cos(aj)
+
+        param=adcirc_parameter_reader("data/test/2d/fort.15")
+        param.read_parameters(NETA=9)
+        param.parameters["NBFR"]=-1
+
+        gr=adcirc_grid_reader("data/test/2d/fort.14")
+        gr.read_grid()
+        nodes,elements,elev_boundary,flow_boundary=gr.get_sets()
+
+        code=Adcirc()
+
+        code._parameters=param.parameters        
+        code.assign_grid_and_boundary(nodes,elements,elev_boundary, flow_boundary)
+
+        code.parameters.use_interface_elevation_boundary=True
+        code.parameters.use_interface_parameters=True
+        code.parameters.use_interface_grid=True
+        code.parameters.A_H=param.parameters["ESLM"] | units.m**2/units.s
+        code.parameters.timestep=abs(param.parameters["DTDP"]) | units.s
+        code.parameters.bottom_friction_law=["linear","quadratic","hybrid"][param.parameters["NOLIBF"]]
+        try:
+          code.parameters.linear_bottom_friction_coeff=param.parameters["TAU"]| units.s**-1
+        except:
+          pass
+        try:
+          code.parameters.quadratic_bottom_friction_coeff=param.parameters["CF"]
+        except:
+          pass
+        code.parameters.use_predictor_corrector=param.parameters["DTDP"]<0
+        code.parameters.use_interface_met_forcing=False
+
+        print code.parameters
+
+        tnow=code.model_time
+        dt=code.parameters.timestep
+  
+        elev_boundaries= list(code.elevation_boundaries())
+  
+        eta61=[]
+        time=[]
+        forcing=[]
+  
+        while tnow<tend-dt/2:
+            elev_boundaries[0].eta=tidal_force_function(tnow+dt/2)
+            code.evolve_model(tnow+dt)
+            tnow=code.get_model_time()
+  
+            eta=code.nodes[60].eta.number
+            time.append(tnow.number)
+            eta61.append(eta)  
+            forcing.append(elev_boundaries[0].eta[0].number)
+      
+        code.stop()
+        
+        from matplotlib import pyplot
+          
+        f=pyplot.figure(figsize=(8,6))
+
+        pyplot.clf()
+        pyplot.plot(time,eta61,'r+')
+        pyplot.plot(time,forcing,'g+')
+        pyplot.plot(time,tidal_force_function((time| units.s)).number)
+        pyplot.show()
+
