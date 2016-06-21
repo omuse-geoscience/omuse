36140eff1ef020ea669f3a6c95e8223a71645903
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2016-06-21 15:49:01 +0200

    q-gcm code import and build system - no interface yet

---
+++ community/q-gcm/interface.f90
@@ -0,0 +1,24 @@
+module qgcm_interface
+
+
+contains
+
+function initialize_code() result(ret)
+  integer :: ret
+  ret=-2
+end function
+function commit_parameters() result(ret)
+  integer :: ret
+  ret=-2
+end function
+function recommit_parameters() result(ret)
+  integer :: ret
+  ret=-2
+end function
+function cleanup_code() result(ret)
+  integer :: ret
+  ret=-2
+end function
+
+
+end module
