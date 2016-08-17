97469f2c151d9c12a14c6e85d3da5663023b4f34
goord <goord@localhost>
2016-08-17 17:20:54 +0200

    added initial dales python wrapper test

---
+++ community/dales/test/test_dales.py
@@ -0,0 +1,37 @@
+from amuse.community import *
+from amuse.test.amusetest import TestWithMPI
+
+from omuse.community.pop.interface import POPInterface
+from omuse.community.pop.interface import POP
+
+from amuse.units import units
+
+#default_options=dict(channel_type="sockets",redirection="none")
+default_options=dict(redirection="none")
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("code").setLevel(logging.DEBUG)
+
+from nose.tools import nottest
+
+class DalesTests(TestWithMPI):
+
+    #test the behavior of the state machine
+    @nottest
+    def test1(self):
+        instance = Dales(**default_options)
+
+        self.assertEquals(instance.state_machine._current_state.name, 'UNINITIALIZED')
+
+        #proceed to evolve
+        time = instance.get_model_time()
+        tend = time + instance.get_timestep_next()
+        instance.evolve_model(tend)
+
+        #check if we are in the expected state
+        self.assertEquals(instance.state_machine._current_state.name, 'EVOLVED')
+
+        instance.stop()
+
+        self.assertEquals(instance.state_machine._current_state.name, 'UNINITIALIZED')
