664235c39762a4105ce68bcad915786604a4b274
Gijs van den Oord <goord@users.noreply.github.com>
2016-08-26 11:27:01 +0200

    Fixed code cleanup in tests

--- community/dales/test/test_dales.py
+++ community/dales/test/test_dales.py
@@ -6,10 +6,10 @@ from omuse.community.dales.interface import Dales
 
 from amuse.units import units
 
-default_options={}
+default_options=dict(redirection="none")
 #default_options=dict(number_of_workers=4)
 #default_options=dict(channel_type="sockets",redirection="none")
-#default_options=dict(redirection="none")
+#default_options=dict(redirection="none",debugger="gdb")
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -25,24 +25,25 @@ class TestDalesInterface(TestWithMPI):
 
         instance = Dales(**default_options)
         instance.cleanup_code()
+
         instance.stop()
-    
+
     def test2(self):
 
-        print "Test 2: default input"
+        print "Test 2: start"
 
-        instance = DalesInterface(**default_options)
-        inputfile,err=instance.get_input_file()
+        instance = Dales(redirection="none",number_of_workers=4)
+        instance.cleanup_code()
 
-        self.assertEquals(inputfile,"namoptions")
-#        instance.cleanup_code()
-#        instance.stop()
+        instance.stop()
     
-    def test3(self):
-
-        print "Test 2: commit params"
-
-        instance = DalesInterface(**default_options)
-#        instance.commit_parameters()
+#    def test2(self):
+#
+#        print "Test 2: default input"
+#
+#        instance = Dales(**default_options)
+#        inputfile,err=instance.get_input_file()
+#
+#        self.assertEquals(inputfile,"namoptions.001")
 #        instance.cleanup_code()
 #        instance.stop()
