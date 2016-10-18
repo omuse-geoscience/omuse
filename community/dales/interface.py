d54eff7f577233c24e27ee2c01ef65d2a1e19e7d
Fredrik Jansson <6065437+fjansson@users.noreply.github.com>
2016-10-18 16:08:47 +0200

    Wrapper functions for getting slab averages and z levels

--- community/dales/interface.py
+++ community/dales/interface.py
@@ -48,7 +48,7 @@ class DalesInterface(CodeInterface,
         returns (dt=0. | units.s)
 
     @remote_function
-    def commit_grid():
+    def commit_grid():        
         pass
 
 # getter functions for vertical profiles - slab averages
@@ -58,27 +58,38 @@ class DalesInterface(CodeInterface,
         returns (out=0.| units.K)
 
     @remote_function(must_handle_array=True)
-    def get_profile_U(k=0):
+    def get_profile_U_(k=0):
         returns (out=0.| units.m / units.s)
 
     @remote_function(must_handle_array=True)
-    def get_profile_V(k=0):
+    def get_profile_V_(k=0):
         returns (out=0.| units.m / units.s)
 
     @remote_function(must_handle_array=True)
-    def get_profile_W(k=0):
+    def get_profile_W_(k=0):
         returns (out=0.| units.m / units.s)
         
     @remote_function(must_handle_array=True)
-    def get_profile_THL(k=0):
+    def get_profile_THL_(k=0):
         returns (out=0.| units.K)
         
     @remote_function(must_handle_array=True)
-    def get_profile_QT(k=0):
+    def get_profile_QT_(k=0):
         returns (out=0.)
-# # #
 
-        
+    @remote_function(must_handle_array=True)
+    def get_zf_(k=0):
+        returns (out=0. | units.m)
+
+    @remote_function(must_handle_array=True)
+    def get_zh_(k=0):
+        returns (out=0. | units.m)
+
+# # #
+    @remote_function()
+    def get_params_grid():
+        returns (i=1, j=1, k=1, xsize=1.0 | units.m, ysize=1.0 | units.m)
+
     @remote_function(must_handle_array=True)
     def get_layer_field(i=0,j=0,k=0):
         returns (temp=0.| units.K)
@@ -91,10 +102,7 @@ class DalesInterface(CodeInterface,
     def evolve_model(tend=0. | units.s):
         pass
 
-    def get_profile(self, field_id):
-        profile = self.get_profile_field(field_id, numpy.arange(1,96))
-        return profile
-
+    
     
 class Dales(CommonCode):
     # these defs match those in daleslib.f90
@@ -103,6 +111,16 @@ class Dales(CommonCode):
     FIELDID_W=3
     FIELDID_THL=4
     FIELDID_QT=5
+    
+    # grid size
+    itot = None
+    jtot = None
+    k    = None
+    xsize = None
+    ysize = None
+
+    
+
     
     def __init__(self,**options):
         CommonCode.__init__(self,  DalesInterface(**options), **options)
@@ -118,6 +136,11 @@ class Dales(CommonCode):
             "namoptions"
         )
 
+
+    def commit_grid(self):        
+        self.overridden().commit_grid()
+        self.get_params()
+        
     def define_state(self, object):
         object.set_initial_state("UNINITIALIZED")
         object.add_transition("UNINITIALIZED", "INITIALIZED", "initialize_code")
@@ -144,3 +167,31 @@ class Dales(CommonCode):
         object.add_transition("RUN", "EVOLVED", "evolve_model", False)
         object.add_method("EVOLVED", "evolve_model")
 
+
+    # wrapping functions for hiding the dummy array passed to getter functions
+    def get_profile_U(self):
+        return self.get_profile_U_(numpy.zeros(self.k))
+
+    def get_profile_V(self):
+        return self.get_profile_V_(numpy.zeros(self.k))
+
+    def get_profile_W(self):
+        return self.get_profile_W_(numpy.zeros(self.k))
+        
+    def get_profile_THL(self):
+        return self.get_profile_THL_(numpy.zeros(self.k))
+        
+    def get_profile_QT(self):
+        return self.get_profile_QT_(numpy.zeros(self.k))
+
+    def get_zf(self):
+        return self.get_zf_(numpy.zeros(self.k))
+
+    def get_zh(self):
+        return self.get_zh_(numpy.zeros(self.k))
+
+
+    # get parameters from the fortran code, store them in the Dales interface object
+    def get_params(self):
+        self.itot,self.jtot,self.k,self.xsize,self.ysize = self.get_params_grid()
+        
