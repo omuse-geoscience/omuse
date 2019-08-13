c17e69656f964ccefd2ebdc98fcdbc28f9148c64
Gijs van den Oord <G.vandenOord@gmail.com>
2019-08-13 11:43:08 +0200

    Renamed and fixed fortran interface test runner and updated documentation

--- src/omuse/community/dales/test/run_dales.f90
+++ src/omuse/community/dales/test/run_dales.f90
@@ -4,13 +4,34 @@ program fortran_test
 
     implicit none
 
-    integer:: stat
-    real(8):: tend
-    real(8):: walltime
+    integer             :: stat, nargs
+    real(8)             :: tend, walltime
+    character(len=256)  :: case, rundir
     stat=0
+
+    nargs = command_argument_count()
+    if(nargs==0) then
+        stop "Please provide a case argument (bomex/rico/sp-testcase/...)"
+    end if
+    call get_command_argument(1, case)
+    rundir="../dales-repo/cases/"//case
+    stat = change_dir(rundir)
+    if(stat/=0) then
+        stop "...failed, stopping."
+    else
+        write(*,*) "..success."
+    endif
 
     write(*,*) "Initializing fortran interface code..."
     stat=initialize_code()
+    if(stat/=0) then
+        stop "...failed, stopping."
+    else
+        write(*,*) "..success."
+    endif
+
+    write(*,*) "Initializing model..."
+    stat=commit_parameters()
     if(stat/=0) then
         stop "...failed, stopping."
     else
