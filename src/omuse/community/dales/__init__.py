59370f0bbc6ea1ac4a4d58a0f759fe92f1b8f5c2
Fredrik Jansson <6065437+fjansson@users.noreply.github.com>
2019-08-28 13:40:32 +0200

    Python3-compatibility of relative imports and data/cases directory

--- src/omuse/community/dales/__init__.py
+++ src/omuse/community/dales/__init__.py
@@ -1,1 +1,3 @@
-# generated file
+# relative import hack
+# https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
