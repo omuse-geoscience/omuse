680fb3d8cec80a101999b35b258ac70cb4d442cb
Merijn Verstraaten <merijn@inconsistent.nl>
2019-10-03 16:12:42 +0200

    Initial implementation of querying the i-emic grid.

--- src/omuse/community/iemic/test_iemic.py
+++ src/omuse/community/iemic/test_iemic.py
@@ -13,3 +13,17 @@ class iemicInterfaceTests(TestWithMPI):
         instance.step()
         instance.cleanup_code()
         instance.stop()
+
+    def test2(self):
+        instance = iemicInterface()
+        instance.initialize()
+        instance.commit_parameters()
+        instance.initialize_code()
+        instance.get_u()
+        instance.get_v()
+        instance.get_w()
+        instance.get_p()
+        instance.get_t()
+        instance.get_s()
+        instance.cleanup_code()
+        instance.stop()
