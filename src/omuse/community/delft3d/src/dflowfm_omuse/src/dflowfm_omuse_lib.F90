a8d75d61246bc965476d01a305013922a4c9ab94
Inti Pelupessy <ipelupessy@users.noreply.github.com>
2019-10-15 19:26:44 +0200

    initial experimental dflowfm interface

---
+++ src/omuse/community/delft3d/src/dflowfm_omuse/src/dflowfm_omuse_lib.F90
@@ -0,0 +1,99 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+module dflowfm_omuse_lib
+  use unstruc_api
+  use unstruc_display, only: jaGUI ! this should be removed when jaGUI = 0 by default
+
+  use m_partitioninfo
+  use m_flow
+  use m_waves
+  use m_ship
+  use m_wind
+  use m_flowgeom
+  use network_data
+  use m_flowexternalforcings
+  use m_partitioninfo
+  use m_sobekdfm
+  use m_transport
+  use m_xbeach_data
+  use dfm_error
+  use m_vegetation
+  use m_sediment
+  use m_integralstats
+  use gridoperations
+  use unstruc_model
+  use unstruc_files
+#ifdef HAVE_MPI
+  use mpi
+#endif
+
+  implicit none
+
+contains
+
+integer function initialize_dflowfm(config_file) result(result)    
+   character(len=*) :: config_file
+
+   ! Extra local variables
+   integer :: inerr  ! number of the initialisation error
+   logical :: mpi_initd
+
+!~    integer(c_int), target, allocatable, save :: x(:,:)
+!~    type(c_ptr) :: xptr
+   integer :: i,j,k
+    
+   result = 0 
+#ifdef HAVE_MPI
+   call mpi_initialized(mpi_initd, inerr)
+   if (.not. mpi_initd) then
+      ja_mpi_init_by_fm = 1
+      call mpi_init(inerr)
+   else
+      ja_mpi_init_by_fm = 0
+   end if
+
+   call mpi_comm_rank(DFM_COMM_DFMWORLD,my_rank,inerr)
+   call mpi_comm_size(DFM_COMM_DFMWORLD,numranks,inerr)
+
+   if ( numranks.le.1 ) then
+      jampi = 0
+   end if
+
+   !   make domain number string as soon as possible
+   write(sdmn, '(I4.4)') my_rank
+
+#else
+   numranks=1
+#endif
+
+   ! do this until default has changed
+   jaGUI = 0
+
+   ! TODO: check why these are needed to avoid a segfault
+   KNX    = 8
+   MXB    = 10
+   MAXLAN = 500
+   MAXPOL = MAXLAN
+
+   call init_core()
+
+   CALL INIDAT()
+   call api_loadmodel(config_file)
+
+   !PETSC must be called AFTER reading the mdu file, so the icgsolver option is known to startpetsc 
+#ifdef HAVE_PETSC
+   call startpetsc()
+#endif
+
+   result = flowinit()
+
+   time_user = tstart_user
+
+   ! Just terminate if we get an error....
+   if (inerr > 0) result=-1
+
+end function initialize_dflowfm
+
+end module
