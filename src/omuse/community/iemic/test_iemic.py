81bb32bcfd98790b33cac5d5c24856d0f68b1cc3
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2020-03-04 11:31:14 +0100

    Fix to work with most recent i-emic.

--- src/omuse/community/iemic/test_iemic.py
+++ src/omuse/community/iemic/test_iemic.py
@@ -6,24 +6,41 @@ from .interface import iemic
 class iemicInterfaceTests(TestWithMPI):
 
     def test1(self):
-        instance = iemicInterface()
-        instance.initialize()
-        instance.commit_parameters()
-        instance.initialize_code()
-        instance.step()
-        instance.cleanup_code()
-        instance.stop()
+        instance = iemicInterface(redirection="none")
+        err = instance.initialize()
+        assert not err, "err should be 0"
+        err = instance.commit_parameters()
+        assert not err, "err should be 0"
+        err = instance.initialize_code()
+        assert not err, "err should be 0"
+        err = instance.step()
+        assert not err, "err should be 0"
+        err = instance.cleanup_code()
+        assert not err, "err should be 0"
+        err = instance.stop()
+        assert not err, "err should be 0"
 
     def test2(self):
-        instance = iemicInterface()
-        instance.initialize()
-        instance.commit_parameters()
-        instance.initialize_code()
-        instance.get_u([0],[0],[0])
-        instance.get_v([0],[0],[0])
-        instance.get_w([0],[0],[0])
-        instance.get_p([0],[0],[0])
-        instance.get_t([0],[0],[0])
-        instance.get_s([0],[0],[0])
-        instance.cleanup_code()
-        instance.stop()
+        instance = iemicInterface(redirection="none")
+        err = instance.initialize()
+        assert not err, "err should be 0"
+        err = instance.commit_parameters()
+        assert not err, "err should be 0"
+        err = instance.initialize_code()
+        assert not err, "err should be 0"
+        val, err = instance.get_u([0],[0],[0])
+        assert not err, "err should be 0"
+        val, err = instance.get_v([0],[0],[0])
+        assert not err, "err should be 0"
+        val, err = instance.get_w([0],[0],[0])
+        assert not err, "err should be 0"
+        val, err = instance.get_p([0],[0],[0])
+        assert not err, "err should be 0"
+        val, err = instance.get_t([0],[0],[0])
+        assert not err, "err should be 0"
+        val, err = instance.get_s([0],[0],[0])
+        assert not err, "err should be 0"
+        err = instance.cleanup_code()
+        assert not err, "err should be 0"
+        err = instance.stop()
+        assert not err, "err should be 0"
