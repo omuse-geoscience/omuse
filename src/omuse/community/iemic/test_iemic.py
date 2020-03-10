606e6f2947202b4d2341e00f1c736a2b109caab9
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2020-03-10 16:12:11 +0100

    Switch to using AMUSE assert.

--- src/omuse/community/iemic/test_iemic.py
+++ src/omuse/community/iemic/test_iemic.py
@@ -6,43 +6,57 @@ from .interface import iemic
 class iemicInterfaceTests(TestWithMPI):
 
     def test1(self):
-        instance = iemicInterface(redirection="none")
+        instance = iemicInterface()
         err = instance.initialize()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.commit_parameters()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.initialize_code()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.step()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.cleanup_code()
-        assert not err, "err should be 0"
-        err = instance.stop()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
+        instance.stop()
 
     def test2(self):
-        instance = iemicInterface(redirection="none")
+        instance = iemicInterface()
         err = instance.initialize()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.commit_parameters()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.initialize_code()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         val, err = instance.get_u([0],[0],[0])
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         val, err = instance.get_v([0],[0],[0])
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         val, err = instance.get_w([0],[0],[0])
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         val, err = instance.get_p([0],[0],[0])
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         val, err = instance.get_t([0],[0],[0])
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         val, err = instance.get_s([0],[0],[0])
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.test_grid("")
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
         err = instance.cleanup_code()
-        assert not err, "err should be 0"
-        err = instance.stop()
-        assert not err, "err should be 0"
+        self.assertEqual(err,0)
+
+        instance.stop()
