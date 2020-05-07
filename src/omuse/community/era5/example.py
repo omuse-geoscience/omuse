1faf07a0502adfe80f7c0583bb68d469bfd00a8b
ipelupessy <i.pelupessy@esciencecenter.nl>
2020-05-07 12:48:22 +0200

    era5 readme, reference and example update

--- src/omuse/community/era5/example.py
+++ src/omuse/community/era5/example.py
@@ -1,38 +1,38 @@
 import time
 
+import matplotlib
+matplotlib.use("TkAgg") # select appropiate backend
+from matplotlib import pyplot
+
 from omuse.units import units
 
 from omuse.community.era5.interface import ERA5
 
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib import pyplot
+e=ERA5(variables=["2m_temperature", "total_precipitation"], 
+       nwse_boundingbox=[70, -15, 40, 15]| units.deg)
 
-e=ERA5(variables=["2m_temperature", "total_precipitation"], nwse_boundingbox=[70, -15, 40, 15]| units.deg, 
-       grid_resolution=1.0 | units.deg, invariate_variables=["land_sea_mask", "orography"])
-
+print("starting date:", e.start_datetime)
 print(e.grid) # note grid has prepended the names with _ (because long names are not valid python var names)
 
-input()
-
-f=pyplot.figure()
+f=pyplot.figure(figsize=(8,8))
 pyplot.ion()
 pyplot.show()
 
-#~ pyplot.imshow(e.grid.land_sea_mask)
-
 dt=1. | units.hour
-tend=2. | units.day
-
+tend=7. | units.day
 
 while e.model_time < tend:
     print("tnow:",e.model_time)
     e.evolve_model(e.model_time+dt)
-    t2=e.grid._2m_temperature
+    val=e.grid._2m_temperature.value_in(units.K)
+    val=0.25*(val[:-1,:-1]+val[1:,:-1]+val[:-1,1:]+val[1:,1:])
+    x=e.grid.lon.value_in(units.deg)
+    y=e.grid.lat.value_in(units.deg)
         
     pyplot.clf()
-    pyplot.imshow(t2.value_in(units.K),vmin=260,vmax=288)
+    pyplot.pcolormesh(x,y,val,vmin=260,vmax=288)
+    pyplot.xlabel("lon (deg)")
+    pyplot.ylabel("lat (deg)")
     f.canvas.flush_events()
 
-    #~ input()
     time.sleep(1/20.)
