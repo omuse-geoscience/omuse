cb4a01d62cbd9e9560abc2c33285605cc3e60c84
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2021-05-26 14:23:04 +0200

    Update OMUSE versioning to work like AMUSE.

--- packages/omuse/setup.py
+++ packages/omuse/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
-from support.version import version, main_version
 from support.classifiers import classifiers
+from setuptools_scm import get_version
 
+version = get_version(
+    root='../..',
+    relative_to=__file__,
+)
 
 name = 'omuse'
 author = 'The AMUSE/ OMUSE team'
@@ -9,17 +13,31 @@ author_email = 'info@amusecode.org'
 license_ = "Apache License 2.0"
 url = 'https://github.com/omuse-geoscience/omuse'
 install_requires = [
-    'omuse-framework>=%s' % main_version,
-    'omuse-qgmodel>=%s' % main_version
+    'omuse-framework>=%s' % version,
+    'omuse-qgmodel>=%s' % version
 ]
 description = 'The Oceanographic Multi-purpose Software Environment: a package for multi-physics and multi-scale earth science simulations. '
 with open("README.md", "r") as fh:
     long_description = fh.read()
 long_description_content_type = "text/markdown"
 
+try:
+    from src.omuse.version import version
+    use_scm_version = False
+    setup_requires = []
+except ImportError:
+    version = False
+    setup_requires = ['setuptools_scm']
+    use_scm_version = {
+        "root": "../..",
+        "relative_to": __file__,
+        "write_to": "src/omuse/version.py",
+    }
+
 setup(
     name=name,
-    version=version,
+    use_scm_version=use_scm_version,
+    setup_requires=setup_requires,
     classifiers=classifiers,
     url=url,
     author_email=author_email,
