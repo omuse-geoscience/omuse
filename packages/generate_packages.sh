4b7e75a684847ac6143655239553a23be09a0414
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2023-09-04 16:38:43 +0200

    Tweak generate_packages

--- packages/generate_packages.sh
+++ packages/generate_packages.sh
@@ -7,6 +7,15 @@ do
   cd $p || exit $?
   rm -rf dist || exit $?
   python setup.py sdist || exit $?
+  cd dist
+  tarname=$(ls *.tar.gz)
+  tar -xzf $tarname
+  pname=$(basename $tarname .tar.gz)
+  toml_file=$(ls $pname/*.toml)
+  echo $toml_file
+  sed -i '/root/d' $toml_file
+  tar -czf $tarname $pname
+  cd -
   cp dist/*.tar.gz ../dist/ || exit $?
   cd .. 
 done
